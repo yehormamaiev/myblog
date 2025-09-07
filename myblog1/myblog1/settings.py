f59417c6f23a2038177dc33cdc658a225356b52c
yehormamaiev <egormamaev0102@gmail.com>
2025-09-07 20:11:48 +0300

    bag fix 0

--- myblog1/myblog1/settings.py
+++ myblog1/myblog1/settings.py
@@ -63,7 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'blog.context_processors.categories_processor',
+                'posts.context_processors.categories_processor',
             ],
         },
     },
--- myblog1/posts/views.py
+++ myblog1/posts/views.py
@@ -1,23 +1,21 @@
 from django.views.generic import ListView, DetailView
-from unicodedata import category
-
 from .models import Category, Post
 
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'posts/home.html'
     context_object_name = 'posts'
     ordering = ['-created_at']
     paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'posts/post_detail.html'
 
 class CategoryListView(ListView):
     model = Post
-    template_name = 'blog/category.html'
+    template_name = 'posts/category.html'
     context_object_name = 'posts'
     paginate_by = 5
 
