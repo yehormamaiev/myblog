49bed4055493e52c7667dd5448fdcd4b5949c717
yehormamaiev <egormamaev0102@gmail.com>
2025-09-07 19:12:28 +0300

    register Category,Post models in admin

--- myblog1/posts/admin.py
+++ myblog1/posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at','is_published')
+    list_filter = ('category', 'is_published', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+
+
